461b591b0700643801fa903ac3e5e52105475fe5
Don McCaughey <don@donm.cc>
2025-05-23 17:28:19 -0700

    Make `GiantsGame` fulfill `Decodable`.
    Change the `__from_json()` method into `decode()`.

--- src/gather/giants_games.py
+++ src/gather/giants_games.py
@@ -4,11 +4,12 @@ import json
 
 from datetime import date, datetime, time
 from importlib import resources
-from serialize import JSONDict, JSONList
+from serialize import Decodable, JSONDict, JSONList
+from typing import Self
 from zoneinfo import ZoneInfo
 
 
-class GiantsGame:
+class GiantsGame(Decodable):
     pacific_time = ZoneInfo('America/Los_Angeles')
 
     def __init__(self, game_date: date, game_time: time | None, opponent: str):
@@ -29,21 +30,22 @@ class GiantsGame:
 
     @classmethod
     def parse_games(cls, schedule_json: JSONList) -> list[GiantsGame]:
-        return [cls.__from_json(game_json) for game_json in schedule_json]
+        return [cls.decode(game_json) for game_json in schedule_json]
 
     def is_today(self, now: datetime) -> bool:
         now = now.astimezone(self.pacific_time)
         return now.date() == self.date
 
-    @staticmethod
-    def __from_json(game_json: JSONDict) -> GiantsGame:
-        dt = datetime.strptime(game_json['date'], '%Y-%m-%d')
+    @classmethod
+    def decode(cls, encoded: JSONDict) -> Self:
+        dt = datetime.strptime(encoded['date'], '%Y-%m-%d')
         date_only = dt.date()
 
-        time_str = game_json['time']
+        time_str = encoded['time']
         if time_str.endswith('PDT'):
             dt = datetime.strptime(time_str[:-4], '%I:%M %p')
             time_only = dt.time()
         else:
             time_only = None
-        return GiantsGame(date_only, time_only, game_json['opponent'])
+
+        return cls(date_only, time_only, encoded['opponent'])
