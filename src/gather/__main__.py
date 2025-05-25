7991e2b1c17f9607dc11f058571cc729b095a990
Don McCaughey <don@donm.cc>
2025-05-24 22:05:37 -0700

    In `gather`, add time periods for API calls.
    Read the previous content when `gather` starts.  Save the last updated
    time with each API service and only update API content when the
    specified time perioud has passed.

--- src/gather/__main__.py
+++ src/gather/__main__.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from gather.weather import Weather
 from utils import atomic_write
 
@@ -12,45 +12,71 @@ from .options import Options
 from .refresh_rate import get_refresh_rate
 
 
+TWO_MINUTES = timedelta(minutes=2)
+HALF_HOUR = timedelta(minutes=30)
+
+
 options = Options.parse()
 
 print('Gathering...')
 
-now = datetime.now()
-updated = now.astimezone(timezone.utc)
-
+now = datetime.now(timezone.utc)
+now_iso_date = now.isoformat(sep=' ', timespec='seconds')
 
 content = {
-    'forecast': {},
+    'forecast': {
+        'updated': '1970-01-01 00:00:00+00:00',
+    },
     'giants_games_today': [],
     'log': get_log(options.web_root),
-    'muni': {},
+    'muni': {
+        'updated': '1970-01-01 00:00:00+00:00',
+    },
     'refresh_rate': get_refresh_rate(now),
     '~source_data': {
         'forecast_response': {},
         'forecast_hourly_response': {},
         'points_response': {},
+        'stop_monitoring_response': {},
     },
-    'updated': updated.isoformat(sep=' ', timespec='seconds'),
+    'updated': now_iso_date,
 }
 
-weather = Weather()
-if weather.get_points():
-    content['~source_data']['points_response'] = weather.points_response
+content_file = options.web_root / 'content/index.json'
+if content_file.exists():
+    with content_file.open('r') as f:
+        content = json.load(f)
+        last_updated = datetime.fromisoformat(content['updated'])
+        content['updated'] = now_iso_date
 
-    if weather.get_forecast():
-        content['~source_data']['forecast_response'] = weather.forecast_response
-        content['forecast']['period_name'] = weather.period_name
-        content['forecast']['detailed_forecast'] = weather.detailed_forecast
+
+last_weather_update = datetime.fromisoformat(content['forecast']['updated'])
+time_since_last_weather_update = now - last_weather_update
+if time_since_last_weather_update >= HALF_HOUR:
+    print('- fetching from api.weather.gov')
+
+    del content['~source_data']['points_response']
+    del content['~source_data']['forecast_response']
+    del content['~source_data']['forecast_hourly_response']
+
+    weather = Weather()
+    if weather.get_points():
+        content['~source_data']['points_response'] = weather.points_response
+
+        if weather.get_forecast():
+            content['~source_data']['forecast_response'] = weather.forecast_response
+            content['forecast']['period_name'] = weather.period_name
+            content['forecast']['detailed_forecast'] = weather.detailed_forecast
+        else:
+            content['forecast']['error'] = weather.error
+
+        if weather.get_forecast_hourly():
+            content['~source_data']['forecast_hourly_response'] = weather.forecast_hourly_response
+        else:
+            content['forecast']['error'] = weather.error
     else:
         content['forecast']['error'] = weather.error
-
-    if weather.get_forecast_hourly():
-        content['~source_data']['forecast_hourly_response'] = weather.forecast_hourly_response
-    else:
-        content['forecast']['error'] = weather.error
-else:
-    content['forecast']['error'] = weather.error
+    content['forecast']['updated'] = now_iso_date
 
 
 giants_games = GiantsGame.get_games()
@@ -63,21 +89,30 @@ else:
     content['giants_games_today'] = []
 
 
-muni_t = Five11(options.five11_org_key)
-if muni_t.get_stop_monitoring():
-    content['~source_data']['stop_monitoring_response'] = muni_t.stop_monitoring_response
+last_muni_update = datetime.fromisoformat(content['muni']['updated'])
+time_since_last_muni_update = now - last_muni_update
+if time_since_last_muni_update >= TWO_MINUTES:
+    print('- fetching from api.511.org')
 
-    if muni_t.expected_arrival_times:
-        content['muni']['expected_arrival_times'] = [
-            arrival_time.isoformat(sep=' ', timespec='seconds')
-            for arrival_time in muni_t.expected_arrival_times
-        ]
-    else:
-        content['muni']['error'] = muni_t.error
+    if 'stop_monitoring_response' in content['~source_data']:
+        del content['~source_data']['stop_monitoring_response']
+
+    muni_t = Five11(options.five11_org_key)
+    if muni_t.get_stop_monitoring():
+        content['~source_data']['stop_monitoring_response'] = muni_t.stop_monitoring_response
+
+        if muni_t.expected_arrival_times:
+            content['muni']['expected_arrival_times'] = [
+                arrival_time.isoformat(sep=' ', timespec='seconds')
+                for arrival_time in muni_t.expected_arrival_times
+            ]
+        else:
+            content['muni']['error'] = muni_t.error
+    content['muni']['updated'] = now_iso_date
 
 
 atomic_write(
-    options.web_root / 'content/index.json',
+    content_file,
     lambda f: json.dump(content, f, indent=4, sort_keys=True),
 )
 
